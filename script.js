faea23aa4863f786e6934468980fea8210f58a85
amin19900 <Da3wa1990@gmail.com>
2025-07-07 14:41:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
+function login() {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-function showSlide(index) {
-  const width = slides[0].clientWidth;
-  document.querySelector('.slides').style.transform = `translateX(-${index * width}px)`;
+  if (username === "" || email === "" || password === "") {
+    alert("الرجاء ملء جميع الحقول");
+  } else {
+    alert("تم تسجيل الدخول بنجاح!");
+    // تغيير هذه الصفحة حسب المكان الذي تريد تحويل المستخدم إليه
+    window.location.href = "index_ar.html";
+  }
 }
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
+function changeLang(lang) {
+  if (lang === 'ar') {
+    window.location.href = "index_ar.html";
+  } else if (lang === 'fr') {
+    window.location.href = "index_fr.html";
+  } else if (lang === 'en') {
+    window.location.href = "index_en.html";
+  }
 }
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(currentIndex);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('nextBtn').onclick = nextSlide;
-  document.getElementById('prevBtn').onclick = prevSlide;
-});
